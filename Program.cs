37cc7acb01a8255726b5cdaa380c1e6acfb37ca7
ST10458042 <161014243+ST10458042@users.noreply.github.com>
2026-03-26 13:04:01 +0200

    Initialized my project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CybersecurityChatBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cybersecurity Awareness Chatbot starting");
+        }
+    }
+}
